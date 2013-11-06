5c17689df8910eed9c16f125fb0d3b1b646f182c
Eric VINCENT <ericvincenterv@gmail.com>
2013-11-06 11:12:07 +0100

    Updating database and CRUD.

--- src/Innova/SelfBundle/Entity/Media.php
+++ src/Innova/SelfBundle/Entity/Media.php
@@ -29,15 +29,36 @@ class Media
     private $description;
 
     /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="url", type="string", length=255)
+     */
+    private $url;
+
+    /**
     * @ORM\ManyToOne(targetEntity="MediaType", inversedBy="medias")
     */
     protected $mediaType;
 
+    /**
+     * To String
+     */
+
+    public function __toString() {
+        return $this->getName();
+    }
 
     /**
      * Get id
      *
-     * @return integer 
+     * @return integer
      */
     public function getId()
     {
@@ -53,14 +74,14 @@ class Media
     public function setDescription($description)
     {
         $this->description = $description;
-    
+
         return $this;
     }
 
     /**
      * Get description
      *
-     * @return string 
+     * @return string
      */
     public function getDescription()
     {
@@ -76,17 +97,63 @@ class Media
     public function setMediaType(\Innova\SelfBundle\Entity\MediaType $mediaType = null)
     {
         $this->mediaType = $mediaType;
-    
+
         return $this;
     }
 
     /**
      * Get mediaType
      *
-     * @return \Innova\SelfBundle\Entity\MediaType 
+     * @return \Innova\SelfBundle\Entity\MediaType
      */
     public function getMediaType()
     {
         return $this->mediaType;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Media
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set url
+     *
+     * @param string $url
+     * @return Media
+     */
+    public function setUrl($url)
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    /**
+     * Get url
+     *
+     * @return string
+     */
+    public function getUrl()
+    {
+        return $this->url;
     }
 }
