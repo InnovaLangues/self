ed0b1e7b3d9690d536d1e651b0a6af9e8861857a
Eric VINCENT <ericvincenterv@gmail.com>
2014-01-13 11:26:52 +0100

    Indentation.

--- src/Innova/SelfBundle/Controller/AjaxController.php
+++ src/Innova/SelfBundle/Controller/AjaxController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 use Symfony\Component\HttpFoundation\Session\Session;
 
 /**
