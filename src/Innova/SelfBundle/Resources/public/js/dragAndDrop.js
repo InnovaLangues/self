e19cf0c3fbdcba575f7b0c34e05a44a7b8522af8
arnaudbey <arnaud.bey@gmail.com>
2014-02-26 11:01:37 +0100

    Drag and Drop working.

--- src/Innova/SelfBundle/Resources/public/js/dragAndDrop.js
+++ src/Innova/SelfBundle/Resources/public/js/dragAndDrop.js
@@ -1,18 +1,27 @@
 $(document).ready(function() {
 	$( ".draggable" ).draggable({
-		snap: true,
-		revert: "invalid"
-	});
+		revert: "invalid",
+		zIndex: 100,
+		start: function (){
+			$(this).addClass('draggable-in-black');
+		},
+		stop: function (){
+			$(this).removeClass('draggable-in-black');
+		}
+	});	
 
 	$( ".droppable" ).droppable({
 		accept: '.draggable',
-		activeClass: "valid-target",
+		activeClass: "ui-state-highlight",
+		hoverClass: "droppable-hover",
 		drop: function( event, ui ) {
 			$(this).droppable('option', 'accept', ui.draggable);
 			var propositionId = ui.draggable.attr('propositionId');
 			$("input[equivalence='"+propositionId+"']").prop("checked", false);
 			$(this).find("input[equivalence='"+propositionId+"']").prop("checked", "checked");
+			$(this).addClass("answered");
 			$("[subquestionId='"+ui.draggable.attr("last-position")+"']").droppable('option', 'accept', '.draggable');
+			$("[subquestionId='"+ui.draggable.attr("last-position")+"']").removeClass("answered");
 			ui.draggable.attr("last-position", $(this).attr("subquestionId"));
 		}
 	});
