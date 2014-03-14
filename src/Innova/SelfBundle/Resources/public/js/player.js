952ed9b8b3020fdad943615d47f9ddfd3e6aabf5
arnaudbey <arnaud.bey@gmail.com>
2014-03-14 11:45:14 +0100

    clean up player.js file

--- src/Innova/SelfBundle/Resources/public/js/player.js
+++ src/Innova/SelfBundle/Resources/public/js/player.js
@@ -1,40 +1,17 @@
-function timestamp(){
-    return Math.round((new Date()).getTime() / 1000);
-}
-
 $(document).ready(function() {
-    /***
-        VAR INIT
-    ****/
+
+    getListenCount();
+
+    /**************
+        GESTION AUDIO
+    **************/
+
     play_in_progress = false;
-    timestampIn = timestamp();
-    listening_count = new Array;
 
-    /***
-        TO KNOW IF I HAVE A SESSION DATA
-    ****/
-    $.ajax({
-        url: Routing.generate('sessionSituationListenNumber'),
-        type: 'GET',
-        dataType: 'json'
-    })
-    .done(function(data) {
-        var number = $("#listening_number").html();
-        if (data.situationListenNumber !== null) {
-            var limit = $('#listening_number').html()
-            var listened = data.situationListenNumber;
-
-            $("#listening_number").html(limit - listened);
-
-            $("#limit_listening_text").html(
-                pluralizeListen(limit, listened)
-            );
-        };
-
-        $('#listens-counter').removeClass('hidden');
-    })
-    .fail(function() {
-        alert('Ajax error 1');
+    $("audio").bind("ended", function(){
+        play_in_progress = false;
+        sound = $(this).attr('id');
+        $(".item_audio_button").css("opacity","1");
     });
 
     $(".item_audio_button").click(function(){
@@ -48,106 +25,30 @@ $(document).ready(function() {
         var audio = document.getElementById(sound);
 
         if(((listened === null || listened <= limit) && listened > 0 || sound != "situation") && !play_in_progress) {
-            if (sound != "situation"){
-                play_in_progress = true;
-                $("#limit_listening_text").html(
-                    pluralizeListen(limit, listened)
-                );
 
-                $(".item_audio_button").css("opacity","0.5");
-                $(this).css("opacity","1");
-                audio.play();
-            }
+            play_in_progress = true;
+            $(".item_audio_button").css("opacity","0.5");
+            $(this).css("opacity","1");
+            audio.play();
 
-            // Cas du clic sur "Situation de départ".
             if (sound === "situation"){
-                play_in_progress = true;
-                $("#limit_listening_text").html(
-                    pluralizeListen(limit, listened)
-                );
-
-                $(".item_audio_button").css("opacity","0.5");
-                $(this).css("opacity","1");
-                audio.play();
-
-                //Increment session
-                $.ajax({
-                    url: Routing.generate('incrementeSessionSituationListenNumber'),
-                    type: 'PUT',
-                    dataType: 'json'
-                })
-
-                .done(function(data) {
-                    var limitListening = $("#limit_listening").html();
-                    var reste = $("#limit_listening").html() - data.situationListenNumber;
-                    var consigne = data.consigneListenNumber;
-                    $("#listening_number").html(reste);
-                    var limit = $("#limit_listening").html();
-                    var listened = data.situationListenNumber;
-                    $("#limit_listening_text").html(
-                        pluralizeListen(limit, listened)
-                    );
-                })
-                .fail(function() {
-                    alert('Ajax error 3');
-                });
-            
+                // $("#limit_listening_text").html( pluralizeListen(limit, listened) );
+                updateListenCount();
             }
         }
     });
-
-    /**************
-        WORD "ECOUTE" DISPLAY WITH OR WITHOUT "s"
-    **************/
-    function pluralizeListen(limit, listened) {
-        if ((limit - listened) < 0){
-            var diff = listened - limit;
-        } else {
-            var diff = limit - listened;
-        }
-        if(diff < 2){
-            return 'écoute';
-        };
-        return 'écoutes';
-    }
-
-    /**************
-        GESTION AUDIO
-    **************/
-    $("audio").bind("ended", function(){
-        play_in_progress = false;
-        sound = $(this).attr('id');
-        $(".item_audio_button").css("opacity","1");
-    });
-
 
     /**************
         FORM  
      **************/
+
+    timestampIn = timestamp();
+
     $("form").submit(function(){
+        resetListenCount();
         totalTime = timestamp() - timestampIn;
         $("#totalTime").val(totalTime);
     });
-
-    /**************
-        TO RESET SESSION VARIABLE IF I CLICK ON "VALIDER" BUTTON
-    **************/
-
-    $("form").submit(function(){
-      $.ajax({
-         url: Routing.generate('resetSessionSituationListenNumber'),
-         async: false,
-         type: 'PUT',
-         dataType: 'json'
-      })
-      .done(function(data) {
-         var reste = data.situationListenNumber;
-      })
-      .fail(function() {
-         alert('Ajax error session');
-      });
-    });
-
 
     /**************
         GESTION DES BADGES SUR LES ONGLETS
@@ -198,4 +99,104 @@ $(document).ready(function() {
         }
     });
 
+
+    /**************
+        WORD "ECOUTE" DISPLAY WITH OR WITHOUT "s"
+    **************/
+
+    function pluralizeListen(limit, listened) {
+        if ((limit - listened) < 0){
+            var diff = listened - limit;
+        } else {
+            var diff = limit - listened;
+        }
+        if(diff < 2){
+            return 'écoute';
+        };
+        return 'écoutes';
+    }
+
+
+    /**************
+       AJAX REQUEST TO GET LISTENING COUNT
+    **************/
+
+    function getListenCount() {
+        $.ajax({
+            url: Routing.generate('sessionSituationListenNumber'),
+            type: 'GET',
+            dataType: 'json'
+        })
+        .done(function(data) {
+            var number = $("#listening_number").html();
+            if (data.situationListenNumber !== null) {
+                var limit = $('#listening_number').html()
+                var listened = data.situationListenNumber;
+
+                $("#listening_number").html(limit - listened);
+
+                $("#limit_listening_text").html(
+                    pluralizeListen(limit, listened)
+                );
+            };
+
+            $('#listens-counter').removeClass('hidden');
+        })
+        .fail(function() {
+            // console.log('Ajax error 1');
+        });
+    }
+
+    /**************
+       AJAX REQUEST TO INCREMENT LISTENING COUNT
+    **************/
+
+    function updateListenCount() {
+        $.ajax({
+            url: Routing.generate('incrementeSessionSituationListenNumber'),
+            type: 'PUT',
+            dataType: 'json'
+        })
+        .done(function(data) {
+            var limitListening = $("#limit_listening").html();
+            var reste = $("#limit_listening").html() - data.situationListenNumber;
+            var consigne = data.consigneListenNumber;
+            $("#listening_number").html(reste);
+            var limit = $("#limit_listening").html();
+            var listened = data.situationListenNumber;
+            $("#limit_listening_text").html( pluralizeListen(limit, listened));
+        })
+        .fail(function() {
+            // console.log('Ajax error 3');
+        });
+    }
+
+
+    /**************
+       AJAX REQUEST TO RESET LISTENING COUNT
+    **************/
+
+    function resetListenCount() {
+        $.ajax({
+             url: Routing.generate('resetSessionSituationListenNumber'),
+             async: false,
+             type: 'PUT',
+             dataType: 'json'
+        })
+        .done(function(data) {
+            var reste = data.situationListenNumber;
+        })
+        .fail(function() {
+            // console.log('Ajax error session');
+        });
+    }
+
+
+    /**************
+        Timestamp function
+    **************/
+    
+    function timestamp(){
+        return Math.round((new Date()).getTime() / 1000);
+    }
 });
