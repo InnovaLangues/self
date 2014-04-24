823c84469c39c8cd04593faed4c66f5bd13a5ddb
arnaudbey <arnaud.bey@gmail.com>
2014-04-24 23:00:23 +0200

    REST editor controller for questionnaire

---
+++ src/Innova/SelfBundle/Controller/Editor/QuestionnaireController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Innova\SelfBundle\Controller\Editor;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Innova\SelfBundle\Entity\Questionnaire;
+
+/**
+ * Questionnaire controller.
+ *
+ * @Route("/editor")
+ */
+class QuestionnaireController extends Controller
+{
+
+    /**
+     * Lists all Questionnaire entities.
+     *
+     * @Route("/questionnaires", name="editor_questionnaire_list")
+     * @Method("GET")
+     * @Template("")
+     */
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $questionnaires = $em->getRepository('InnovaSelfBundle:Questionnaire')->findAll();
+
+        return array(
+            'questionnaires' => $questionnaires
+        );
+    }
+
+    /**
+     * Finds and displays a Questionnaire entity.
+     *
+     * @Route("/questionnaires/{id}", name="editor_questionnaire_show")
+     * @Method("GET")
+     * @Template("")
+     */
+    public function showAction($id)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $questionnaire = $em->getRepository('InnovaSelfBundle:Questionnaire')->find($id);
+
+        if (!$questionnaire) {
+            throw $this->createNotFoundException('Unable to find Questionnaire entity.');
+        }
+
+        return array(
+            'questionnaire' => $questionnaire
+        );
+    }
+
+    /**
+     * Creates a new Questionnaire entity.
+     *
+     * @Route("/questionnaires", name="editor_questionnaire_create")
+     * @Method("POST")
+     * @Template("")
+     */
+    public function createAction(Request $request)
+    {
+        $questionnaire = new Questionnaire();
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($entity);
+        $em->flush();
+
+        $this->get('session')->getFlashBag()->add(
+                    'notice',
+                    'Le questionnaire a été créé.'
+                );
+
+        return $this->  redirect($this->generateUrl(
+                            'editor_questionnaire_show',
+                            array('id' => $entity->getId()))
+                        );
+    }
+
+    /**
+     * Updates a Questionnaire entity
+     *
+     * @Route("/questionnaires/{id}", name="editor_questionnaire_edit")
+     * @Method("PUT")
+     * @Template("")
+     */
+    public function editAction($id)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $questionnaire = $em->getRepository('InnovaSelfBundle:Questionnaire')->find($id);
+
+        if (!$questionnaire) {
+            throw $this->createNotFoundException('Unable to find Questionnaire entity.');
+        }
+
+        $this->get('session')->getFlashBag()->add(
+                    'notice',
+                    'Le questionnaire a été créé.'
+                );
+
+        return $this->redirect($this->generateUrl(
+                        'editor_questionnaire_show',
+                        array('id' => $entity->getId()))
+                    );
+    }
+
+}
