6f1239e9ed408bf1bf8676469f1577e2029e786d
arnaudbey <arnaud.bey@gmail.com>
2014-04-25 00:20:38 +0200

    WIP REST

--- src/Innova/SelfBundle/Controller/Editor/QuestionnaireController.php
+++ src/Innova/SelfBundle/Controller/Editor/QuestionnaireController.php
@@ -22,9 +22,9 @@ class QuestionnaireController extends Controller
      *
      * @Route("/questionnaires", name="editor_questionnaire_list")
      * @Method("GET")
-     * @Template("")
+     * @Template("InnovaSelfBundle:Editor:list.html.twig")
      */
-    public function listAction()
+    public function listQuestionnairesAction()
     {
         $em = $this->getDoctrine()->getManager();
 
@@ -40,7 +40,7 @@ class QuestionnaireController extends Controller
      *
      * @Route("/questionnaires/{id}", name="editor_questionnaire_show")
      * @Method("GET")
-     * @Template("")
+     * @Template("InnovaSelfBundle:Editor:show.html.twig")
      */
     public function showAction($id)
     {
@@ -105,6 +105,35 @@ class QuestionnaireController extends Controller
         $this->get('session')->getFlashBag()->add(
                     'notice',
                     'Le questionnaire a été créé.'
+                );
+
+        return $this->redirect($this->generateUrl(
+                        'editor_questionnaire_show',
+                        array('id' => $entity->getId()))
+                    );
+    }
+
+     /**
+     * Delete a Questionnaire entity
+     *
+     * @Route("/questionnaires/{id}", name="editor_questionnaire_delete")
+     * @Method("DELETE")
+     * @Template("")
+     */
+    public function deleteAction($id)
+    {
+
+        $em = $this->getDoctrine()->getManager();
+
+        $questionnaire = $em->getRepository('InnovaSelfBundle:Questionnaire')->find($id);
+
+        if (!$questionnaire) {
+            throw $this->createNotFoundException('Unable to find Questionnaire entity.');
+        }
+
+        $this->get('session')->getFlashBag()->add(
+                    'notice',
+                    'Le questionnaire a été supprimé.'
                 );
 
         return $this->redirect($this->generateUrl(
