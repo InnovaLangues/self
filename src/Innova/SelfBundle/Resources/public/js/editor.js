cacf1c862d85e6539809b2aa34a6a76cca6041d8
arnaudbey <arnaud.bey@gmail.com>
2014-07-02 15:14:43 +0200

    add questionnaire status support

--- src/Innova/SelfBundle/Resources/public/js/editor.js
+++ src/Innova/SelfBundle/Resources/public/js/editor.js
@@ -21,6 +21,10 @@ $(document).ready(function() {
         setTypology(questionnaireId);
     });
 
+    $('#status').on('change',function(e){
+        setStatus(questionnaireId);
+    });
+
     $('#fixed-order').on('change',function(e){
         isChecked = $('#fixed-order').prop('checked');
         setFixedOrder(isChecked);
@@ -332,6 +336,24 @@ function setTypology(questionnaireId) {
         afterAjax();
     }); 
 }
+
+function setStatus(questionnaireId) {
+    beforeAjax();
+    $.ajax({
+        url: Routing.generate('editor_questionnaire_set-status'),
+        type: 'POST',
+        dataType: 'json',
+        data: { 
+            questionnaireId: questionnaireId,
+            status: $("#status").val() 
+        }
+    })
+    .done(function(data) {
+        $("#status").val(data.status);
+        afterAjax();
+    }); 
+}
+
 
 function setFixedOrder(isChecked){
     beforeAjax();
