dd0180597326e49a49d7cd1c8a7f1a12f090b47e
arnaudbey <arnaud.bey@gmail.com>
2014-07-07 11:06:37 +0200

    WIP mediaClick controller

---
+++ src/Innova/SelfBundle/Controller/Player/MediaClickController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Innova\SelfBundle\Controller\Player;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Innova\SelfBundle\Entity\MediaClick;
+
+/**
+ * MediaClick controller.
+ *
+ * @Route("/ajax")
+ */
+class AjaxController extends Controller
+{
+
+    /**
+     * @Route("/increment-media-click", name="increment-media-click", options={"expose"=true})
+     * @Method("GET")
+     */
+    public function incrementMediaClickAction()
+    {
+        $request = $this->get('request');
+        $em = $this->getDoctrine()->getManager();
+
+        $media = $em->getRepository('InnovaSelfBundle:Media')->find($request->request->get('mediaId'));
+        $test = $em->getRepository('InnovaSelfBundle:Test')->find($request->request->get('testId'));
+        $questionnaire = $em->getRepository('InnovaSelfBundle:Questionnaire')->find($request->request->get('questionnaireId'));
+        $user = $this->get('security.context')->getToken()->getUser();
+
+        $mediaClick = new MediaClick();
+        $mediaClick->setMedia($media);
+        $mediaClick->setUser($user);
+        $mediaClick->setTest($test);
+        $mediaClick->setQuestionnaire($questionnaire);
+
+        $em->persist($mediaClick);
+        $em->flush();
+    }
+
+
+    private function getMediaClickCount($media, $test, $questionnaire)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $user = $this->get('security.context')->getToken()->getUser();
+
+        $mediaClicks = $em->getRepository('InnovaSelfBundle:MediaClick')->findBy(array(
+                                                                                    'media' => $media,
+                                                                                    'test' => $test,
+                                                                                    'questionnaire' => $questionnaire
+                                                                                ));
+
+        return count($mediaClicks);
+    }
+
+
+    /**
+     * @Route("/is-media-playable", name="is-media-playable", options={"expose"=true})
+     * @Method("GET")
+     */
+    public function isMediaPlayableAction()
+    {
+        $isPlayable = false;
+
+        $request = $this->get('request');
+        $em = $this->getDoctrine()->getManager();
+
+        $media = $em->getRepository('InnovaSelfBundle:Media')->find($request->request->get('mediaId'));
+        $test = $em->getRepository('InnovaSelfBundle:Test')->find($request->request->get('testId'));
+        $questionnaire = $em->getRepository('InnovaSelfBundle:Questionnaire')->find($request->request->get('questionnaireId'));
+        $user = $this->get('security.context')->getToken()->getUser();
+
+
+        $nbClick = $this->getMediaClickCount($media, $test, $questionnaire);
+        $mediaLimit = $em->getRepository('InnovaSelfBundle:MediaLimit')->findBy(array(
+                                                                                    'media' => $media,
+                                                                                    'test' => $test,
+                                                                                    'questionnaire' => $questionnaire
+                                                                                ));
+
+       
+        if($mediaLimit || $mediaLimit > $nbClick){
+            $isPlayable = true;
+        }
+
+        return new JsonResponse(
+            array(
+                'isPlayable' => $isPlayable,
+            )
+        );
+
+    }
+
+
+}
