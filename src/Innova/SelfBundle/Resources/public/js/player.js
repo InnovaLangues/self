f5e3c74e4f594a3de9ab0ff02c9ba34142a177e7
Eric VINCENT <ericvincenterv@gmail.com>
2014-07-07 11:30:54 +0200

    CSS et JS pilote3.

--- src/Innova/SelfBundle/Resources/public/js/player.js
+++ src/Innova/SelfBundle/Resources/public/js/player.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     getListenCount();
     uncheckEverything();
     checkBadges();
+    checkSelect();
 
     /**************
         GESTION AUDIO
@@ -68,6 +69,11 @@ $(document).ready(function() {
 
     $(":checkbox, :radio").change(function(){
         checkBadges();
+    });
+
+    /** pour le cas EEC, afin d'activer ou pas le bouton "Submit" **/
+    $(".check-select").change(function(){
+        checkSelect();
     });
 
 
@@ -195,7 +201,6 @@ function playMedia(media, btn){
 /**************
    CHECK BADGES
 **************/
-
 function checkBadges(){
     var incomplete_tab = 0;
     $( ".tab-pane" ).each(function( index ) {
@@ -210,19 +215,33 @@ function checkBadges(){
     });
 
     if (incomplete_tab == 0) {
-    alert("remove");
         $("#submit").removeAttr("disabled", "disabled");
     } else {
-    alert("attr");
         $("#submit").attr("disabled", "disabled");
     }
 }
 
+/**************
+   CHECK SELECT
+   pour le cas EEC, afin d'activer ou pas le bouton "Submit"
+**************/
+function checkSelect(){
+    var complete_select = true;
+    $( ".check-select" ).each(function( index ) {
+        if ( $(this).val() == '' ) {
+            complete_select = false;        }
+    });
+
+    if (complete_select) {
+        $("#submit").removeAttr("disabled", "disabled");
+    } else {
+        $("#submit").attr("disabled", "disabled");
+    }
+}
 
 /**************
     WORD "ECOUTE" DISPLAY WITH OR WITHOUT "s"
 **************/
-
 function pluralizeListen(limit, listened) {
     if ((limit - listened) < 0){
         var diff = listened - limit;
