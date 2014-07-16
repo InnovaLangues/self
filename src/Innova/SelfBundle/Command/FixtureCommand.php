48fbf52baa5f953080845e0002845c847675906d
arnaudbey <arnaud.bey@gmail.com>
2014-07-16 11:51:54 +0200

    logs

--- src/Innova/SelfBundle/Command/FixtureCommand.php
+++ src/Innova/SelfBundle/Command/FixtureCommand.php
@@ -233,7 +233,7 @@ class FixtureCommand extends ContainerAwareCommand
             }
 
             $editorLogObjects = array(
-                "contexte", "objet de la question", "question", "proposition", 
+                "contexte", "texte", "objet de la question", "question", "proposition", 
                 "reponse", "syllable", "clue", "instruction", "functional-instruction", 
                 "comment", "feedback", "distractor", "app-paire", "app-media", "app-answer",
                 "app-distractor", "listening-limit", "clue-type", "task"
