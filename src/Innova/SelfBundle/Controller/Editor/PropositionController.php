3eb428ce2c8c4cd7b5fa26a5d81fb7d9d6b93f53
arnaudbey <arnaud.bey@gmail.com>
2014-10-06 11:20:15 +0200

    proposition controller no longer extends controller in order to use manager method

--- src/Innova/SelfBundle/Controller/Editor/PropositionController.php
+++ src/Innova/SelfBundle/Controller/Editor/PropositionController.php
@@ -3,17 +3,39 @@
 namespace Innova\SelfBundle\Controller\Editor;
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 /**
- * Proposition Controller for editor
- *
- * @Route("admin/editor")
+ * Class PropositionController
+ * @Route(
+ *      "admin/editor",
+ *      name    = "",
+ *      service = "innova_editor_proposition"
+ * )
  */
-class PropositionController extends Controller
+class PropositionController
 {
+    protected $propositionManager;
+    protected $entityManager;
+    protected $templating;
+    protected $request;
+
+    public function __construct($propositionManager, $entityManager, $templating) 
+    {
+        $this->propositionManager = $propositionManager;
+        $this->entityManager = $entityManager;
+        $this->templating = $templating;
+    }
+
+    public function setRequest(Request $request = null)
+    {
+        $this->request = $request;
+
+        return $this;
+    }
+
     /**
      *
      * @Route("/questionnaires/toggle_right_answer", name="editor_questionnaire_toggle_right_anwser", options={"expose"=true})
@@ -21,23 +43,16 @@ class PropositionController extends Controller
      */
     public function toggleRightAnswserAction()
     {
-        $request = $this->get('request');
-        $em = $this->getDoctrine()->getManager();
+        $em = $this->entityManager;
+        $request = $this->request->request;
 
-        $questionnaire = $em->getRepository('InnovaSelfBundle:Questionnaire')->find($request->request->get('questionnaireId'));
-        $propositionId = $request->request->get('propositionId');
+        $questionnaire = $em->getRepository('InnovaSelfBundle:Questionnaire')->find($request->get('questionnaireId'));
+        $propositionId = $request->get('propositionId');
         $proposition = $em->getRepository('InnovaSelfBundle:Proposition')->find($propositionId);
 
-        if ($proposition->getRightAnswer() === true) {
-            $proposition->setRightAnswer(false);
-        } else {
-            $proposition->setRightAnswer(true);
-        }
+        $proposition = $this->propositionManager->toggleRightAnswer($proposition);
 
-        $em->persist($proposition);
-        $em->flush();
-
-        $template = $this->renderView('InnovaSelfBundle:Editor/partials:proposition.html.twig',array('questionnaire' => $questionnaire, 'proposition' => $proposition));
+        $template = $this->templating->render('InnovaSelfBundle:Editor/partials:proposition.html.twig',array('questionnaire' => $questionnaire, 'proposition' => $proposition));
 
         return new Response($template);
     }
