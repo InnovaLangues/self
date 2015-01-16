98b86114004dac07501a5b9af664d8c4c4c19840
Eric VINCENT <ericvincenterv@gmail.com>
2015-01-16 10:25:25 +0100

    Varnish user context test

--- app/AppCache.php
+++ app/AppCache.php
@@ -2,8 +2,20 @@
 
 require_once __DIR__.'/AppKernel.php';
 
-use FOS\HttpCacheBundle\SymfonyCache\EventDispatchingHttpCache;
+//use FOS\HttpCacheBundle\SymfonyCache\EventDispatchingHttpCache;
+
+use FOS\HttpCache\SymfonyCache\EventDispatchingHttpCache;
+use FOS\HttpCache\SymfonyCache\UserContextSubscriber;
 
 class AppCache extends EventDispatchingHttpCache
 {
+    /**
+     * Overwrite constructor to register event subscribers for FOSHttpCache.
+     */
+    public function __construct(HttpKernelInterface $kernel, $cacheDir = null)
+    {
+        parent::__construct($kernel, $cacheDir);
+
+        $this->addSubscriber(new UserContextSubscriber());
+    }
 }
