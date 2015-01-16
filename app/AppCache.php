4009f0d0e416605835d9f36196b7c68a2adb3b1a
Eric VINCENT <ericvincenterv@gmail.com>
2015-01-16 10:28:09 +0100

    Second user hash test

--- app/AppCache.php
+++ app/AppCache.php
@@ -2,9 +2,9 @@
 
 require_once __DIR__.'/AppKernel.php';
 
-//use FOS\HttpCacheBundle\SymfonyCache\EventDispatchingHttpCache;
+use FOS\HttpCacheBundle\SymfonyCache\EventDispatchingHttpCache;
 
-use FOS\HttpCache\SymfonyCache\EventDispatchingHttpCache;
+//use FOS\HttpCache\SymfonyCache\EventDispatchingHttpCache;
 use FOS\HttpCache\SymfonyCache\UserContextSubscriber;
 
 class AppCache extends EventDispatchingHttpCache
