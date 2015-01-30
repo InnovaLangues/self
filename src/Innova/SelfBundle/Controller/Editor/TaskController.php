3abfe1bf220e0ea8934bfabc1eecbf852eacd1d4
Eric VINCENT <ericvincenterv@gmail.com>
2015-01-30 13:58:51 +0100

    Test /editor/questionnaire/X

--- src/Innova/SelfBundle/Controller/Editor/TaskController.php
+++ src/Innova/SelfBundle/Controller/Editor/TaskController.php
@@ -139,6 +139,8 @@ class TaskController
      */
     public function showAction($questionnaireId, $testId)
     {
+
+        die("editor_questionnaire_show");
         $em = $this->entityManager;
 
         $questionnaire = $em->getRepository('InnovaSelfBundle:Questionnaire')->find($questionnaireId);
