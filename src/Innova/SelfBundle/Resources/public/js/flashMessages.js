f7ecfd8d8a7a31cd84c557c3bed4b9866ed09936
Donovan Tengblad <purplefish32@gmail.com>
2015-05-13 17:42:49 +0200

    Debug

--- src/Innova/SelfBundle/Resources/public/js/flashMessages.js
+++ src/Innova/SelfBundle/Resources/public/js/flashMessages.js
@@ -1,19 +1,40 @@
 function showFlash()
 {
+    console.log("finding cookies");
+    
     var cookie = $.cookie("flashes");
 
+    console.log(cookie);
+
     if (!cookie) {
+        console.log("Not found");
         return;
     }
 
+    console.log("Not found");
+
     var flashes = JSON.parse(cookie);
+
+    console.log("Flashes");
+
+    console.log(flashes);
     
     for (var i = flashes.length - 1; i >= 0; i--) {
+        console.log(i);
         $('.flash-messages').append('<div class="alert alert-' + flashes[i][0] + '">' + flashes[i][1] + '</div>');
         //console.log(flashes[i]);
     };
 
+    console.log("End for");
+
+    console.log("Removing cookie");
+
     $.removeCookie('flashes', { path: '/' });
+
+    console.log("Removed");
+
+    console.log($.cookie("flashes"));
+
 }
 
 $(document).ready(function() {
