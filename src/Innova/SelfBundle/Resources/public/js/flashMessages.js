d37c8ed10b23dfe3859f3babfff997d34dac5fbc
Donovan Tengblad <purplefish32@gmail.com>
2015-05-13 17:59:11 +0200

    Changed messages

--- src/Innova/SelfBundle/Resources/public/js/flashMessages.js
+++ src/Innova/SelfBundle/Resources/public/js/flashMessages.js
@@ -19,12 +19,12 @@ function showFlash()
 
     console.log("Length");
 
-    console.log(flashes.length);
+    console.log(flashes.info.length);
     
-    for (var i = flashes.length - 1; i >= 0; i--) {
+    for (var i = flashes.info.length - 1; i >= 0; i--) {
         console.log(i);
-        $('.flash-messages').append('<div class="alert alert-' + flashes[i][0] + '">' + flashes[i][1] + '</div>');
-        //console.log(flashes[i]);
+        $('.flash-messages').append('<div class="alert alert-info">' + flashes[i][1] + '</div>');
+        console.log(flashes[i]);
     };
 
     console.log("End for");
