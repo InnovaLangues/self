30d480b0ca2d8942d9f5d0d22c667eadc0c914b5
Donovan Tengblad <purplefish32@gmail.com>
2015-05-13 17:52:00 +0200

    Test

--- src/Innova/SelfBundle/Controller/Features/AdminUserController.php
+++ src/Innova/SelfBundle/Controller/Features/AdminUserController.php
@@ -171,6 +171,9 @@ class AdminUserController extends Controller
             $form = $this->get("self.user.manager")->handleForm($user, $request);
             if (!$form) {
                 $this->get("session")->getFlashBag()->set('info', "L'utilisateur a bien été créée");
+
+                #test
+                $this->get("session")->getFlashBag()->set('info', "L'utilisateur a bien été créée -- test");
                 
                 $cacheManager = $this->get('fos_http_cache.cache_manager');
                 $cacheManager->invalidateRoute('admin_user');
