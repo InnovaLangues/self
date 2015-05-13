cbfe325c988c7653be28231aa475f616b1ae676c
Donovan Tengblad <purplefish32@gmail.com>
2015-05-13 17:37:45 +0200

    Changed flash messages

--- src/Innova/SelfBundle/Resources/public/js/flashMessages.js
+++ src/Innova/SelfBundle/Resources/public/js/flashMessages.js
@@ -1,31 +1,19 @@
 function showFlash()
 {
-    //var cookie = getCookie("flashes"); // fos_http_cache.flash_message.name
-    console.log("Looking for cookies");
-
-    console.log($.cookie());
-
     var cookie = $.cookie("flashes");
 
     if (!cookie) {
-        console.log("pas trouvé le cookie");
-        console.log(cookie);
         return;
     }
 
-    console.log("trouvé le cookie");
-    console.log(cookie);
-
     var flashes = JSON.parse(cookie);
-    console.log(flashes);
-    // show flashes in your DOM...
+    
     for (var i = flashes.length - 1; i >= 0; i--) {
-        console.log(flashes[i]);
+        $('.flash-messages').append('<div class="alert alert-' + flashes[i][0] + '">' + flashes[i][1] + '</div>');
+        //console.log(flashes[i]);
     };
 
-    document.cookie = "flashes=; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
-
-    console.log("Cookie monster FTW!");
+    $.removeCookie('flashes', { path: '/' });
 }
 
 $(document).ready(function() {
