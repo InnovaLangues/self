799eb0f92f0667dd7110ed63ed08475bc184e49e
arnaudbey <arnaud.bey@gmail.com>
2015-05-18 09:41:50 +0200

    factorize flashmessage js

--- src/Innova/SelfBundle/Resources/public/js/flashMessages.js
+++ src/Innova/SelfBundle/Resources/public/js/flashMessages.js
@@ -8,21 +8,12 @@ function showFlash()
 
     var flashes = JSON.parse(cookie);
 
-    for (var i = flashes.danger.length - 1; i >= 0; i--) {
-        $('.flash-messages').append('<div class="alert alert-danger">' + flashes.danger[i] + '</div>');
-    };
-
-    for (var i = flashes.warning.length - 1; i >= 0; i--) {
-        $('.flash-messages').append('<div class="alert alert-warning">' + flashes.warning[i] + '</div>');
-    };
-
-    for (var i = flashes.success.length - 1; i >= 0; i--) {
-        $('.flash-messages').append('<div class="alert alert-success">' + flashes.success[i] + '</div>');
-    };
-
-    for (var i = flashes.info.length - 1; i >= 0; i--) {
-        $('.flash-messages').append('<div class="alert alert-info">' + flashes.info[i] + '</div>');
-    };
+    $.each(flashes, function( index, value ) {
+        var msgs = flashes[index];
+        for (var i = msgs.length - 1; i >= 0; i--) {
+            $('.flash-messages').append('<div class="alert alert-'+index+'">' + msgs[i] + '</div>');
+        };
+    });
 
     $.removeCookie('flashes', { path: '/' });
 }
