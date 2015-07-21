debec5e8172cb05bb72176c319eaaf45e9d12cf4
arnaudbey <arnaud.bey@gmail.com>
2015-07-21 08:42:42 +0200

    add/edit some originStudents in fixtures

--- src/Innova/SelfBundle/Command/FixtureCommand.php
+++ src/Innova/SelfBundle/Command/FixtureCommand.php
@@ -39,8 +39,8 @@ class FixtureCommand extends ContainerAwareCommand
         $levelManager->create(array("A1", "A1+", "A2", "A2+", "B1", "B1+", "B2", "B2+", "C1", "C1+"));
 
         $originStudentManager = $this->getContainer()->get("self.originStudent.manager");
-        $originStudentManager->create(array("LANSAD Valence", "IFSI Valence", "LLCE", "LEA", "UJF", "IUT2", "Formation continue", "Lycée Vaucanson", "Université de Nice", "Autres"));
-        $originStudentManager->update(array("LANSAD" => "LANSAD Grenoble" ));
+        $originStudentManager->create(array("LANSAD Valence", "IFSI Valence", "LLCE Valence", "LEA", "UJF", "IUT2", "Formation continue", "Lycée Vaucanson", "Université de Nice", "Autres"));
+        $originStudentManager->update(array("LANSAD" => "LANSAD Grenoble", "LLCE" => "LLCE Grenoble" ));
 
         $languageManager = $this->getContainer()->get("self.language.manager");
         $languageManager->create(array("English", "Italian", "Chinese", "Spanish", "Japanese"));
