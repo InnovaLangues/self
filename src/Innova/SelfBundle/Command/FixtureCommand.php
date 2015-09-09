7ede8a226565ca1f07efe63b68e6609d2f845e47
arnaudbey <arnaud.bey@gmail.com>
2015-09-09 15:02:40 +0200

    edit fixtures

--- src/Innova/SelfBundle/Command/FixtureCommand.php
+++ src/Innova/SelfBundle/Command/FixtureCommand.php
@@ -39,7 +39,7 @@ class FixtureCommand extends ContainerAwareCommand
         $levelManager->create(array("A1", "A1+", "A2", "A2+", "B1", "B1+", "B2", "B2+", "C1", "C1+"));
 
         $originStudentManager = $this->getContainer()->get("self.originStudent.manager");
-        $originStudentManager->create(array("MEEF Savoie", "MEEF Valence", "ICM", "LANSAD Valence", "IFSI Valence", "LLCE Valence", "LEA", "IUT2", "Formation continue", "Lycée Vaucanson", "Université de Nice", "Autres"));
+        $originStudentManager->create(array("MEEF Savoie", "MEEF Valence", "ICM", "LANSAD Valence", "IFSI Valence", "LLCE Valence", "LEA", "IUT2", "Formation continue", "Lycée Vaucanson", "Université de Nice", "Université Lyon 1", "Autres"));
         $originStudentManager->update(array("LANSAD" => "LANSAD Grenoble", "LLCE" => "LLCE Grenoble", "UJF" => "MEEF Grenoble"));
 
         $languageManager = $this->getContainer()->get("self.language.manager");
