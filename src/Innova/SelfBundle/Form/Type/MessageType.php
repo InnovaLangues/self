6959e3e0ca6b15de069185c3390594102e1141a3
arnaudbey <arnaud.bey@gmail.com>
2015-10-13 16:46:41 +0200

    add send message feature

---
+++ src/Innova/SelfBundle/Form/Type/MessageType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Innova\SelfBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class MessageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('message', 'text', array(
+            'attr' => array('class' => 'form-control'),
+            'label'  => 'message.content',
+            'translation_domain' => 'messages',
+        ));
+
+        $builder->add('channel', 'choice', array(
+            'choices'   => array('all' => 'A tous', 'admin' => 'Aux admins uniquement'),
+            'label'  => 'message.channel',
+            'attr' => array('class' => 'form-control'),
+            'translation_domain' => 'messages',
+        ));
+
+        $builder->add('save', 'submit', array(
+            'attr' => array('class' => 'btn btn-default'),
+            'label'  => 'generic.validate',
+            'translation_domain' => 'messages',
+        ));
+    }
+
+    public function getName()
+    {
+        return 'message';
+    }
+}
