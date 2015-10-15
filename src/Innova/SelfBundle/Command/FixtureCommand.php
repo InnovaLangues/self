ad734b5404696f645f1950f958cb221391ab7288
beya <beya@DA2013178>
2015-10-15 15:40:12 +0200

    add some institution for register form

--- src/Innova/SelfBundle/Command/FixtureCommand.php
+++ src/Innova/SelfBundle/Command/FixtureCommand.php
@@ -39,7 +39,11 @@ class FixtureCommand extends ContainerAwareCommand
         $levelManager->create(array("A1", "A1+", "A2", "A2+", "B1", "B1+", "B2", "B2+", "C1", "C1+"));
 
         $originStudentManager = $this->getContainer()->get("self.originStudent.manager");
-        $originStudentManager->create(array("MEEF Savoie", "MEEF Valence", "MEEF Grenoble", "ICM", "LANSAD Grenoble", "LANSAD Valence", "IFSI Valence", "LLCE Valence", "LLCE Grenoble", "LEA", "IUT2", "Formation continue", "Lycée Vaucanson", "Université de Nice", "Université Lyon 1", "Autres"));
+        $originStudentManager->create(array(
+            "MEEF Savoie", "MEEF Valence", "MEEF Grenoble", "ICM", "LANSAD Grenoble", "LANSAD Valence", 
+            "IFSI Valence", "LLCE Valence", "LLCE Grenoble", "LEA", "IUT2", "Formation continue", "Lycée Vaucanson", 
+            "Université de Nice", "Université Lyon 1", "Université de Strasbourg", 
+            "Ecole Doctorale Grenoble", "Ecole d'Architecture Grenoble", "Autres"));
 
         $languageManager = $this->getContainer()->get("self.language.manager");
         $languageManager->create(array("English", "Italian", "Chinese", "Spanish", "Japanese"));
