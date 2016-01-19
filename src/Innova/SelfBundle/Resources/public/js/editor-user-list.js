e10478563eb2198c5f6d63db912259766db79c16
arnaudbey <arnaud.bey@gmail.com>
2016-01-19 09:40:07 +0100

    add btn

--- src/Innova/SelfBundle/Resources/public/js/editor-user-list.js
+++ src/Innova/SelfBundle/Resources/public/js/editor-user-list.js
@@ -1,3 +1,14 @@
 $(function() {
     $('input#search').quicksearch('.users-table tbody tr');
 });
+
+
+$(function() {
+	$(".delete-trace").click(function(event) {
+		if ($(this).attr("href") == "#"){
+			event.preventDefault();
+			$(this).attr('href',$(this).attr('realpath'));
+	 		$(this).html(" Confimer !");
+	 	}
+	});
+});
