34319815a0063f69386b6d3fb124981a66df7cf1
arnaudbey <arnaud.bey@gmail.com>
2017-07-11 09:29:59 +0200

    add route to make some request

--- src/Innova/SelfBundle/Controller/ApiController.php
+++ src/Innova/SelfBundle/Controller/ApiController.php
@@ -6,11 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Innova\SelfBundle\Entity\Media\MediaClick;
 
+/**
+ * Class ApiController.
+ *
+ * @Route(
+ *      "api/",
+ * )
+ */
 class ApiController extends Controller
 {
     /**
-     * @Route("api/_stats", name = "api-stats")
+     * @Route("_stats", name = "api-stats")
      * @Method("GET")
      */
     public function getStatsAction()
@@ -29,6 +37,44 @@ class ApiController extends Controller
 
         $response = new JsonResponse();
         $response->setData($data);
+
+        return $response;
+    }
+
+    /**
+     * @Route("_evoliatis", name = "evoliatis")
+     * @Method("GET")
+     */
+    public function testEvoliatisAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        // some selects
+        $users = [1439, 20, 27256, 3693];
+        $medias = [315, 316, 317, 318, 320, 324, 331];
+        $tests = [298, 240, 251, 177];
+        $sessions = [365, 366, 367];
+        $questionnaires = [7056,6051, 6251];
+
+
+        $user = $em->getRepository('InnovaSelfBundle:User')->find($users[array_rand($users)]);
+        $media = $em->getRepository('InnovaSelfBundle:Media\Media')->find($medias[array_rand($medias)]);
+        $test = $em->getRepository('InnovaSelfBundle:Test')->find($tests[array_rand($tests)]);
+        $session = $em->getRepository('InnovaSelfBundle:Session')->find($sessions[array_rand($sessions)]);
+        $questionnaire = $em->getRepository('InnovaSelfBundle:Questionnaire')->find($questionnaires[array_rand($questionnaires)]);
+
+        // and an insert
+        $mediaClick = new MediaClick;
+        $mediaClick->setMedia($media);
+        $mediaClick->setUser($user);
+        $mediaClick->setTest($test);
+        $mediaClick->setSession($session);
+        $mediaClick->setQuestionnaire($questionnaire);
+
+        $em->persist($mediaClick);
+        $em->flush();
+
+        $response = new JsonResponse();
 
         return $response;
     }
