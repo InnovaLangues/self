5eb794440b27602f6aaef902ad1771eb21f3b441
Arnaud Bey <arnaud.bey@gmail.com>
2017-10-16 11:50:52 +0200

    replace exists by join in repository (#1084)

--- src/Innova/SelfBundle/Repository/PropositionRepository.php
+++ src/Innova/SelfBundle/Repository/PropositionRepository.php
@@ -22,42 +22,24 @@ class PropositionRepository extends EntityRepository
 
     public function getByUserTraceAndSubquestion($subquestion, $user, $component, $session)
     {
+        $dql = "SELECT p FROM Innova\SelfBundle\Entity\Proposition p
+                INNER JOIN p.answers a
+                LEFT JOIN a.trace t
+                WHERE p.subquestion = :subquestion
+                AND t.user = :user
+                AND t.session = :session";
+
         if ($component) {
-            $dql = "SELECT p FROM Innova\SelfBundle\Entity\Proposition p
-                WHERE p.subquestion = :subquestion
-                AND EXISTS (
-                    SELECT a FROM Innova\SelfBundle\Entity\Answer a
-                    LEFT JOIN a.trace t
-                    WHERE a.subquestion = :subquestion
-                    AND a.proposition = p
-                    AND t.user = :user
-                    AND t.session = :session
-                    AND t.component = :component
-                )
-            ";
+            $dql .= " AND t.component = :component";
+        }
 
-            $query = $this->_em->createQuery($dql)
-                ->setParameter('subquestion', $subquestion)
-                ->setParameter('session', $session)
-                ->setParameter('component', $component)
-                ->setParameter('user', $user);
-        } else {
-            $dql = "SELECT p FROM Innova\SelfBundle\Entity\Proposition p
-                WHERE p.subquestion = :subquestion
-                AND EXISTS (
-                    SELECT a FROM Innova\SelfBundle\Entity\Answer a
-                    LEFT JOIN a.trace t
-                    WHERE a.subquestion = :subquestion
-                    AND a.proposition = p
-                    AND t.user = :user
-                    AND t.session = :session
-                )
-            ";
-
-            $query = $this->_em->createQuery($dql)
+        $query = $this->_em->createQuery($dql)
                 ->setParameter('subquestion', $subquestion)
                 ->setParameter('session', $session)
                 ->setParameter('user', $user);
+
+        if ($component) {
+            $query->setParameter('component', $component);
         }
 
         return $query->getResult();
