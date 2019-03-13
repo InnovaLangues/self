06c0f68cdf111bbf5fd4f97ca04b9d47598c0a74
jcrombez <jeremy.crombez@kibatic.com>
2019-03-13 16:40:19 +0100

    fix keyvalue manager

--- src/Innova/SelfBundle/Manager/KeyValueManager.php
+++ src/Innova/SelfBundle/Manager/KeyValueManager.php
@@ -21,7 +21,7 @@ class KeyValueManager
         $keyValue = $this->repository->findOneByKey($key);
 
         if ($keyValue === null) {
-            return $defaultValue;
+            return new KeyValue($key, $defaultValue);
         }
 
         return $keyValue;
