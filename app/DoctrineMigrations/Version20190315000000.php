4b53362604ab3ac1573d5d0e7ef77903ff4f7e38
jcrombez <jeremy.crombez@kibatic.com>
2019-03-15 14:08:03 +0100

    fix migration class name

--- app/DoctrineMigrations/Version20190315000000.php
+++ app/DoctrineMigrations/Version20190315000000.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20190314152835 extends AbstractMigration
+class Version20190315000000 extends AbstractMigration
 {
     /**
      * @param Schema $schema
